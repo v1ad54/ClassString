95c6070f5af7fc6ff44b0a4deaf49fab90a2fb74
Chizhov Vladislav <v1ad54@yandex.ru>
2018-10-07 23:45:06 +0700

    Создание строк

---
+++ src/ClassString.java
@@ -0,0 +1,51 @@
+
+public class ClassString {
+
+	public static void main(String[] args) {
+		String str0 = "Создание строк.";
+		System.out.println(str0);
+		for (int i = 0; i < str0.length(); i++) {
+			System.out.print("=");
+		}
+		System.out.println();
+
+		// Создание строки
+		String str1 = "Это просто строка.";
+		System.out.println("\n" + str1);
+
+		// Создание массива строк
+		String[] strArr = { "Строка 1", "Строка 2", "Строка 3" };
+		System.out.println("\n" + "Массив строк:");
+		for (int i = 0; i < strArr.length; i++) {
+			System.out.println("\t" + strArr[i]);
+		}
+
+		// Пустая строка
+		System.out.println("\n" + "Пустая строка.");
+		String strNull = null;
+		if (strNull == null) {
+			System.out.println("strNull is null");
+		}
+		String strEmpty = new String();
+		if (strEmpty.length() == 0) {
+			System.out.println("strEmpty.length() == 0");
+		}
+		if (strEmpty.isEmpty()) {
+			System.out.println("strEmpty is empty");
+		}
+		if (strEmpty.equals("")) {
+			System.out.println("strEmpty equals \"\"");
+		}
+		System.out.println("strEmpty.compareTo(\"\"): " + strEmpty.compareTo(""));
+
+		// Создание строки через массив символов
+		char[] chars = { 'A', 'B', 'C', 'D', 'E' };
+		String strChars = new String(chars);
+		System.out.println(strChars);
+		// Указание диапазона символьного массива
+		String strChars2 = new String(chars, 1, 3); // Начало диапазона и количество используемых символов
+		System.out.println(strChars2);
+
+	}
+
+}
